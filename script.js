45ec5ba71863bc0802a7b063e0aeca7dba0dee0b
Suhan-Shrestha <scubashrestha4@gmail.com>
2025-01-05 15:21:58 +0545

    first

---
+++ script.js
@@ -0,0 +1,115 @@
+// let x=2;
+// console.log(x**2)
+// console.log(2**3)
+// console.log(!1)
+// let fname="suhan";
+// let lname="shrestha";
+// // let fullname="my name is"+" " +fname + " " +lname;
+// // console.log(fullname)
+// // let fullname=`my name is ${fname} ${lname}`
+// // console.log(fullname)
+// let course="csit";
+// let fullpara=`my name is ${fname} ${lname} i am studying ${course} 1st semester`
+// console.log(fullpara)
+// let num=4;
+// num += 1
+// console.log(num)
+// let age=16;
+// if(age>=18){
+//     console.log("you are an adult")
+// }
+// else{
+//     console.log("you are a minor")
+// }
+// let num1=6;
+// let num2=4;
+// if(num1>num2){
+//     console.log(`${num1} is greater`)
+// }else{
+//     console.log(`${num2} is greater`)
+// }
+// let num="1";
+// switch(num){
+//     case"1":
+//     console.log("number is 1");
+//     break;
+//     case"2":
+//     console.log("number is 2")
+//     break;
+//     case"3":
+//     console.log("num is 3")
+//     break;
+//     default:
+//         console.log("num is not given")
+
+
+// }
+
+
+
+// let num1=Number(prompt("enter first number"));
+// let num2=Number(prompt("enter second number"));
+// let operator= prompt("enter the operator:");
+
+// switch(operator)
+// {
+//     case "+":
+//         console.log(num1+num2)
+//         break;
+//         case "-":
+//             console.log(num1-num2)
+//             break;
+//             case "*":
+//                 console.log(num1*num2)
+//                 break; 
+//                 case "/":
+//                     console.log(num1/num2)
+//                     break;
+//                     default:
+//                         console.log("num is not given")
+
+        
+// }
+
+// let num=Number(prompt("enter a number"));
+// switch(num)
+// {
+// case 1:
+//          console.log("sunday")
+//          break;
+//         case 2:
+
+//         console.log("monday")
+//              break;
+//              case 3:
+//                  console.log("tuesday")
+//                  break; 
+//                  case 4:
+//                      console.log("wednesday")
+//                      break;
+//                      case 5:
+//                         console.log("thursday")
+//                         break;
+//                         case 6:
+//                             console.log("friday")
+//                             break;
+//                             case 7:
+//                                 console.log("saturday")
+//                                 break;
+//                      default:
+//                          console.log("num is not given")
+// }
+
+
+let num1=5;
+for(let i; i<10;i++)
+    {
+
+}
+
+
+
+
+
+  
+
